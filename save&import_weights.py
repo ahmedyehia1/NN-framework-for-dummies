0df062fe939e7b2267d196ccccfc29432a6bca33
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-01-24 00:11:25 +0200

    Update save&import_weights.py
    adding comments

--- save&import_weights.py
+++ save&import_weights.py
@@ -1,13 +1,34 @@
 import numpy as np
 
 def save_weights(layer_arr):
+    
+    '''
+        saves 2d array into csv file:
+        Args:
+            layer_arr: a 2d array holds numbers
+        Shape:
+            - Input: layer_arr (N, m) N: number of weights
+            - Output: None
+        Examples:
+            save_weights(np.array([[1,2,3,4,56,9,7],[3,4,6,7,8]]))
+        '''
+    
     f = open('saved_weights.csv', 'w')
     for layer in layer_arr:
         f.write(",".join(str(x) for x in layer) + "\n")
 
-# save_weights(np.array([[1,2,3,4,56,9,7],[3,4,6,7,8]]))
-
 def import_saved_weights():
+    
+    '''
+        import 2d array from csv file:
+        Args: None
+        Shape:
+            - Input: None
+            - Output: layer_arr (N, m) N: number of weights
+        Examples:
+            import_saved_weights()
+    '''    
+    
     all_of_the_weights = open('saved_weights.csv', 'r').read()
     lines = all_of_the_weights.split('\n')
     layer_arr = []
@@ -18,5 +39,3 @@ def import_saved_weights():
             y = xx.astype(np.float)
             layer_arr.append(y)
     return layer_arr
-
-# print(import_saved_weights())
